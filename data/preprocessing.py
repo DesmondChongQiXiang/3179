b3ae7528f99f4b7ba2c9e99715deba7f4c9b3fbd
DesmondChongQiXiang <dcho0043@student.monash.edu>
2024-10-10 09:10:49 +0800

    update visualisation

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,16 +1,27 @@
 import pandas as pd
 
 # Load the CO2 emissions data
-co2_data_path = 'combined_co2_continents_2022.csv'  # Update with your actual file path
+co2_data_path = 'world_emissions_by_sector.csv'  # Update with your actual file path
 co2_data = pd.read_csv(co2_data_path)
 
-# Group the data by region and calculate the total emissions for each region
-grouped_data = co2_data.groupby('Continent', as_index=False).agg({
-    'Annual CO₂ emissions': 'sum'  # Sum emissions for each region
-})
+sector_mapping = {
+    "Carbon dioxide emissions from buildings": "Buildings",
+    "Carbon dioxide emissions from bunker fuels": "Bunker Fuels",
+
+    "Carbon dioxide emissions from electricity and heat": "Electricity & Heat",
+    "Carbon dioxide emissions from land use change and forestry": "Land Use",
+    "Carbon dioxide emissions from other fuel combustion": "Other Fuels",
+    "Carbon dioxide emissions from transport": "Transport",
+    "Carbon dioxide emissions from manufacturing and construction": "Manufacturing",
+    "Fugitive emissions of carbon dioxide from energy production": "Fugitive Emissions",
+    "Carbon dioxide emissions from industry": "Industry"
+}
+
+# Replace sector names
+co2_data['Sector'] = co2_data['Sector'].replace(sector_mapping)
 
 # Display the grouped data
-print(grouped_data)
+print(co2_data)
 
 # Optionally, save the grouped data to a new CSV file
-grouped_data.to_csv('grouped_co2_emissions_by_region.csv', index=False)
+co2_data.to_csv('1world_emissions_by_sector.csv', index=False)
